734769639bc2d0976afebc1f50697ff79e88e3f8
Kaustubh Singh <kaustubh.physics@gmail.com>
2026-06-03 16:47:36 +0530

    Tested and polished the Pauli list interface.
    	1. Added a add term method to MultiSiteOperator
    	2. Changed MultiSiteOperator.terms from list to dict
    	3. Added to_matrix() method to term and MultiSiteOperator
    	4. Added a testing dir
    	5. Tested the Pauli interface against SparsePauliOp

--- operators.py
+++ operators.py
@@ -1,24 +1,33 @@
 import numpy as np
 from pauli import pauli
 
+# *************************************************************************
+
+# I have hardcoded 2 level systems in this implementation (eg. spin chains)
+# In a later version, generalise this to more than 2 levels.
+
+# *************************************************************************
+
 def op_to_matrix(op):
-    # for ease of use, allowing op = (coefficient, pauli_character) to be passed
+    # for ease of use, allowing op = (pauli_character, coefficient) to be passed
     # will be used in add* methods of MultiSiteOperator
     if not isinstance(op, np.ndarray):
-        if op[1].upper() in pauli.keys():       # op[1] = pauli character, op[0] = coef
-            matrix = pauli[op[1].upper()]*op[0]
+        if op[0].upper() in pauli.keys():       # op[0] = pauli character, op[1] = coef
+            matrix = pauli[op[0].upper()]*op[1]
         else:
-            raise ValueError(f"{op[1]} is not a valid pauli character.")
+            raise ValueError(f"{op[0]} is not a valid pauli character.")
     else:
         matrix = np.array(op)
     return matrix               # tested.
 
-def array_to_pauli_list(arr):
+def array_to_pauli_list_general(arr):
     '''
     Takes in an ndarray of shape (2,2)
     returns the corresponding Pauli list
 
     np.ndarray([[1,1],[1,1]]) -> [ ['X',1], ['I',1] ]
+    
+    Works for all matrices in GL(2)
     '''
     assert arr.shape == (2,2), f'arr.shape is {arr.shape}, has to be (2,2).'
     a, b, c, d = arr[0,0], arr[0,1], arr[1,0], arr[1,1]
@@ -40,16 +49,52 @@ def array_to_pauli_list(arr):
     ]
     
     filtered_out = []
-    # keeping coeffs which are close to zero
+    # keeping coeffs which are not close to zero
+    for i, p_chr in enumerate(out):
+        if np.abs(p_chr[1]) > 1e-6:
+            filtered_out.append([p_chr[0], p_chr[1]])
+
+    return filtered_out             # tested.
+
+def array_to_pauli_list_hermitian(arr):
+    '''
+    Takes in an ndarray of shape (2,2)
+    returns the corresponding Pauli list
+
+    np.ndarray([[1,1],[1,1]]) -> [ ['X',1], ['I',1] ]
+    
+    For Hermitian matrices. 
+    Little bit faster than the general method.
+    '''
+    assert arr.shape == (2,2), f'arr.shape is {arr.shape}, has to be (2,2).'
+    a, b, c, d = arr[0,0], arr[0,1], arr[1,0], arr[1,1]
+    I_coeff = .5 *(a+d)
+    Z_coeff = .5 *(a-d)
+
+    # if arr is Hermitian, we can simplify X_coeff and Y_coeff.
+    # this speedup would be at the cost of accuracy/ stability.
+    # test what happens when you do that.
+    X_coeff = .5 *(b+c)               # b.real
+    Y_coeff = .5j*(b-c)               #-b.imag
+    
+    # should I make all these real here itself?
+    out = [  
+        [ 'I', I_coeff ],
+        [ 'X', X_coeff ],
+        [ 'Y', Y_coeff ],
+        [ 'Z', Z_coeff ]
+    ]
+    
+    filtered_out = []
+    # keeping coeffs which are not close to zero
     # raising error if there is a large imaginary part.
     for i, p_chr in enumerate(out):
-        if p_chr[1].imag > 1e-6:
+        if abs(p_chr[1].imag) > 1e-6:
             raise ValueError(f'The coefficient of {p_chr[0]} has an imaginary part of {p_chr[1].imag}')
         if np.abs(p_chr[1]) > 1e-6:
             filtered_out.append([p_chr[0], p_chr[1].real])
 
     return filtered_out             # tested.
-
 def _push_at_idx_string(p_string_0, p_string_1, idx):
     len_pauli_string = len(p_string_0)
     
@@ -79,40 +124,30 @@ def _pauli_dict_to_pauli_list(p_dict):
     '''
     return [[pauli_string, coeff] for pauli_string, coeff in p_dict.items()]
 
-def _add_pauli_lists(p_list_0, p_list_1):
-    # If you create a pauli list/ dict class, this can be the __add__ method.
-    '''
-    p_list_0 = [['XI',1] ['IX',3]]
-    p_list_1 = [['XI',2] ['IZ',3]]
-    
-    -->
+def _add_pauli_lists(p_list_0, p_list_1):                                           # IMPORTANT SIMPLE SPEED UP: DONT ADD TERMS. JUST APPEND.
+    p_dict_0 = _pauli_list_to_pauli_dict(p_list_0)                                  # Do we even need to group terms together like this?
+    p_dict_1 = _pauli_list_to_pauli_dict(p_list_1)                                  # sure it looks prettier, but SparsePauliOp does not care if we pass [['X',2]] or [['X',1],['X',1]]. 
+                                                                                    # both are treated the same by SparsePauliOp. 
+    basis_set_0 = set(p_dict_0.keys())                                              # This grouping together business could be taking time 
+    basis_set_1 = set(p_dict_1.keys())                                              # as it is MultiSiteOperator.to_pauli_list() is quite time consuming (order 1 sec for large num sites).
+                                                                                    # TEST IF USING APPEND INSTEAD OF ADDING HAS NOTABLE IMPACE ON SPEED.
+    shared_pauli_strings = basis_set_0 & basis_set_1
+    exclusive_0          = basis_set_0 - basis_set_1
+    exclusive_1          = basis_set_1 - basis_set_0
 
-    [['XI',3],['IX',3], ['IZ',3]]
-    '''
-    p_dict_0 = _pauli_list_to_pauli_dict(p_list_0)
-    p_dict_1 = _pauli_list_to_pauli_dict(p_list_1)
-    
-    basis_set_0 = set(p_dict_0.keys())
-    basis_set_1 = set(p_dict_1.keys())
-    
-    shared_pauli_strings    = basis_set_0 & basis_set_1     # set intersection
-    exclusive_pauli_strings = basis_set_0 ^ basis_set_1     # set symmetric difference
-    # look into how ^ is implemented. If internally it calculates the set intersection 
-    # and then substracts it from the union then diy because you already calculated ^.
-    summed_dict = {} 
+    summed_dict = {}
+
     for ps in shared_pauli_strings:
         coeff_sum = p_dict_0[ps] + p_dict_1[ps]
-        if abs(coeff_sum)>1e-6:
-            summed_dict = {ps: p_dict_0[ps] + p_dict_1[ps]}
+        if abs(coeff_sum) > 1e-6:
+            summed_dict[ps] = coeff_sum         
 
-    # would it be faster to take the difference of the intersection with
-    # basis_set_0 and basis_set_1 getting two different sets and then running two differnt for loops?
-    # we would not have to put in the conditiional in that case.
-    for ps in exclusive_pauli_strings:
-        if ps in p_dict_0:
-            summed_dict[ps] = p_dict_0[ps]
-        else:
-            summed_dict[ps] = p_dict_1[ps]
+    for ps in exclusive_0:
+        summed_dict[ps] = p_dict_0[ps]
+
+    for ps in exclusive_1:
+        summed_dict[ps] = p_dict_1[ps]
+
     return _pauli_dict_to_pauli_list(summed_dict)
 
 def _push_at_idx_list(pauli_list, p_string:str, idx):
@@ -163,16 +198,23 @@ def tensor_pauli_lists(p_list0, p_list1, idx):
 
 class term:
     """
-    Represents a single operator term, e.g. {0: Z_matrix, 2: X_matrix} → ZIXI...
+    Represents a single operator term, e.g. {0: Z_matrix, 2: X_matrix} -> ZIX
     Makes the dict hashable so it can live in a set.
 
     would it be better to just inherit the dict class?
     probably not since we dont want to mess with immutability.
     """
-    def __init__(self, site_ops: dict):
+    def __init__(self, site_ops: dict, num_sites = None):
         # site_ops: {site_index (int): operator (np.ndarray)}
         self.site_ops = site_ops                    # keep the dict for easy access
-        self.num_sites = max(site_ops.keys()) + 1
+        supported_site_idxs = site_ops.keys()
+        self.supported_site_idxs = supported_site_idxs
+        if num_sites is None:
+            self.num_sites = max(supported_site_idxs) + 1
+        else:
+            self.num_sites = num_sites
+        
+        self.num_supported_sites = len(supported_site_idxs)
         self._hash = hash(
             frozenset(
                 (site, arr.tobytes()) for site, arr in site_ops.items()
@@ -217,7 +259,7 @@ class term:
     def keys(self):
         return self.site_ops.keys()
 
-    def to_pauli_list(self, num_sites = None):
+    def to_pauli_list(self):
         # will this be recomputed on each call?
         # if so, is there a way of storing this untill there is a change?
         '''
@@ -226,12 +268,13 @@ class term:
 
         {0:[[1,1],[1,1]], 1:[[1,0],[0,-1]]} -> [('XZ',1), ('IZ',1)]
         '''
+        num_sites = self.num_sites
 
         indiv_pauli_decomp = {}
         
         # decomposing the term into pauli matrices along with site index
         for site_idx, arr in self.site_ops.items():
-            indiv_pauli_decomp[site_idx] = array_to_pauli_list(arr)
+            indiv_pauli_decomp[site_idx] = array_to_pauli_list_hermitian(arr)   # IF COMPLEX DTYPE TROUBLE, CHANGE THIS TO array_to_pauli_list_general()
         '''
         (2X^(0) + 3Z^(0)) \otimes (5Y^(2) - I^(2))
         <-->
@@ -253,6 +296,7 @@ class term:
         
         # Initial I padding
         smallest_site_idx = list(indiv_pauli_decomp.keys())[0] 
+        largest_site_idx =  list(indiv_pauli_decomp.keys())[-1] 
         initial_idtt_padding = 'I' * smallest_site_idx
 
         pauli_list_out = [
@@ -263,10 +307,26 @@ class term:
             pauli_list_out = tensor_pauli_lists(pauli_list_out, pauli_list, site_idx)
 
         # tail I padding
-        if num_sites is not None:
-            final_idtt_padding = 'I'*(num_sites - self.num_sites)
-            return [[pt[0]+final_idtt_padding, pt[1]] for pt in pauli_list_out]           # test more.
-        return pauli_list_out
+        final_idtt_padding = 'I'*(num_sites - largest_site_idx - 1)
+        return [[pt[0]+final_idtt_padding, pt[1]] for pt in pauli_list_out]         
+
+    def to_matrix(self):
+        '''
+        returns the Kronecker producted matrix
+        '''
+        if 0 in self.supported_site_idxs:
+            out = self.site_ops[0]
+        else:
+            out = np.eye(2)
+
+        for i in range(1,self.num_sites):
+            if i in self.supported_site_idxs:
+                out = np.kron(out, self.site_ops[i])
+            else:
+                out = np.kron(out,np.eye(2))
+            
+        return out
+
     def __repr__(self):
         # maybe change this to give the pauli list form
         return f"{self.site_ops}"
@@ -279,7 +339,7 @@ class MultiSiteOperator:
     Would be compatible with TTNs and hTTNs.
     '''
 
-    def __init__(self, num_sites, num_term_types):
+    def __init__(self, num_sites):
         '''
         num_terms_list = [
                             num_single_site_terms,
@@ -294,10 +354,10 @@ class MultiSiteOperator:
         num_term_types = 2  (single site and two site terms)
         num_sites = N
         '''
-        self.num_term_types = num_term_types
         self.num_sites = num_sites
         
-        self.terms = [set() for _ in range(num_term_types)]    # would it be faster if this was a dict?
+        #self.terms = [set() for _ in range(num_term_types)]    # would it be faster if this was a dict?
+        self.terms = {}
         '''
         self.terms[0] would be a set of all the single site terms
         self.terms[1] would be a set of all two site terms and so on
@@ -310,6 +370,40 @@ class MultiSiteOperator:
         corresponds to ZIXII...I
         I have given the pauli str instead of ndarray for brevity
         '''
+    def add_term(self, trm):
+        '''
+        adds the term trm to the MultiSiteOperator
+        ensures that the num_sites attribuite is inherited
+        '''
+        
+        # One issue is that if we add the same term twice, 
+        # it won't count the second one. FIX. 
+        # throw error if trm has support on more sites than self. FIX.
+
+        idx = trm.num_supported_sites 
+        trm.num_sites = self.num_sites
+        
+        if idx not in self.terms.keys():
+            self.terms[idx] = set()
+
+        self.terms[idx].add(trm)
+
+    def add_all_terms(self, trms_dict):
+        '''
+        updates self.terms by passing the terms dict 
+
+        mso_terms = {
+                1: { term({0:x}), term({1:x}), term({2:x})
+                },
+
+                2: { term({0: z, 1:z}), term({1:z, 2:z})
+                }
+        }
+        '''
+        for num_supported_sites, trm_set in trms_dict.items():
+            for trm in trm_set:
+                self.add_term(trm)
+
 
     def add_uniform_single_site_terms(self, op):
         '''
@@ -317,9 +411,12 @@ class MultiSiteOperator:
         '''
         matrix = op_to_matrix(op)
 
+        if 1 not in self.terms.keys():
+            self.terms[1] = set()
+
         for site_idx in range(self.num_sites):
-            term = term({site_idx: matrix})
-            self.terms[0].add(term)                         # self.terms[i] is a set
+            trm = term({site_idx: matrix},num_sites = self.num_sites)
+            self.terms[1].add(trm)                         # self.terms[i] is a set
 
     def add_uniform_local_two_site_terms(self, op, pbc = False):
         '''
@@ -329,46 +426,68 @@ class MultiSiteOperator:
         O^(0)O^(1) + O^(1)O^(2) + ... + O^(N-2)O^(N-1) + O^(N-1)O^(0)   if pbc = True
         '''
         matrix = op_to_matrix(op) 
-        assert num_sites > 1, f'Need at least two sites to add two sites terms, you have {self.num_sites}'
+        assert self.num_sites > 1, f'Need at least two sites to add two sites terms, you have {self.num_sites}'
+
+        if not 2 in self.terms.keys():
+            self.terms[2] = set()
+
         for site_idx in range(self.num_sites - 1):
-            term = term({site_idx: matrix, site_idx+1: matrix})
-            self.terms[1].add(term)
+            trm = term({site_idx: matrix, site_idx+1: matrix}, num_sites = self.num_sites)
+            self.terms[2].add(trm)
 
         if pbc:
-            term = term({self.num_sites-1: matrix, 0: matrix})
-            self.terms[1].add(term)
+            trm = term({self.num_sites-1: matrix, 0: matrix},num_sites = self.num_sites)
+            self.terms[2].add(trm)
     
     def unfold(self):
         '''
         returns a set with all the term objects
         '''
         out = []
-        for trm_set in self.terms:
+        for num_supported_sites, trm_set in self.terms.items():
             for trm in trm_set:
                 out.append(trm)
         return set(out)
+    
+    def flush(self):
+        self.terms = {}
 
-    def to_pauli_list(self):        # Not working correctly. Run for mso.
-        # will have to define how to take the sum of pauli lists whcih have pauli strings of the same length
+    def to_pauli_list(self):                                                                                # SEE IF THIS COULD BE SPED UP. TAKES TOO LONG. 
+        
         pauli_list_out = [] 
 
         # converting terms which have support on the same number of sites
         # into pauli lists and then adding them up
-        for trm_set in self.terms:
+        for num_supported_sites,trm_set in self.terms.items():
             _sum = []
             for trm in trm_set:
-                print(_sum,trm.to_pauli_list(self.num_sites))
-                _sum = _add_pauli_lists(_sum, trm.to_pauli_list(self.num_sites))
-            print('\n')
+                _sum = _add_pauli_lists(_sum, trm.to_pauli_list())
+
             pauli_list_out += _sum
 
         return pauli_list_out
-        
+
+    def to_matrix(self):
+        '''
+        converts the MultiSiteOperator object
+        to a Kronecker producted matrix.
+        '''
+        N = self.num_sites
+        _sum = np.zeros([2**N, 2**N], dtype = complex)        # Change this dtype to complex if casting issues occur.
+        unfolded_terms = self.unfold()
+        for trm in unfolded_terms:
+            _sum += trm.to_matrix()
+
+        return _sum
+
+    
+    def __str__(self):
+        return str(self.to_pauli_list())
     # we can have add_uniform_local_k_site_terms
-    # add_global_k_site_terms (all to all k site interactions)
+    # add_global_k_site_terms (all to all k site interactionsr
     # LMG has all to all two site interactions
 
-    # for the __str__ function, return the analytical version 
+    # for the __repr__ function, return the analytical version 
     # like X^(0) + X^(1) + Z^(0)Z^(1) etc
 
     # could also include a function that add terms or instantiates using pauli_lists or SparsePauliOp
@@ -385,16 +504,23 @@ if __name__ == '__main__':
     y = pauli['Y']
     z = pauli['Z']
     so = {4:np.ones([2,2]), 1:z, 2:(x+y+z+np.ones([2,2])+ np.eye(2)*7)}
+    so1 = {0: np.array([[3,1],[2,1]]) 
+
+           }
 
     pauli_list = [('XII', -1), ('IXI', -1), ('IIX', -1), ('ZZI', -1), ('IZZ', -1), ('ZIZ', -1)]
 
-    mso_terms = [
-            { term({0:x}), term({1:x}), term({2:x})
+    mso_terms = {
+            1: { term({0:x}), term({1:x}), term({2:x})
                 },
-            { term({0: z, 1:z}), term({1:z, 2:z})
+            2: { term({0: z, 1:z}), term({1:z, 2:z})
                 }
-    ]
+            }
     # There needs to be a better interface for instantiating MultiSiteOperator.
 
-    mso = MultiSiteOperator(3,1)
-    mso.terms = mso_terms
+    mso = MultiSiteOperator(3)
+
+    mso1 = MultiSiteOperator(4)
+    mso2 = MultiSiteOperator(4)
+    trm = term({0:y, 1:np.eye(2), 3:z})
+    trm1 = term({0:y, 1:np.ones((2,2)), 2:z})
